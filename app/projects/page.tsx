cb878373b5ac19cdfaa6ea38976e866ae4940c75
JKs162426 <jesus20jk@gmail.com>
2026-07-16 22:59:00 -0600

    Dynamic data fetching

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,17 @@
-export default function Projects() {
-  return <h1>Projects Overview</h1>;
+import { getProjects } from "@/lib/projects-db";
+
+export default async function Projects() {
+  const projects = await getProjects();
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-2">Projects Overview</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id} className="mb-1">
+            {project.title} - {project.type}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
